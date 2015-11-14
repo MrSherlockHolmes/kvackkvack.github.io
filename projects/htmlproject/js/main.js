02913e4cdc56e45753f4beea9f32def391b4b053
kvackkvack <friskarthur@gmail.com>
2015-11-14 21:16:02 +0100

    Update main.js

--- projects/htmlproject/js/main.js
+++ projects/htmlproject/js/main.js
@@ -3,26 +3,25 @@ function main() {
   var p = addNewElement("p", "player");
   p.id = "player";
   
+  setInterval(mainLoop, 1000/30);
+  
   var eventDoc, doc, body, mouseX, mouseY;
-  document.onmousemove = function() {
-    event = event || window.event;
-    eventDoc = (event.target && event.target.ownerDocument) || document;
-    doc = eventDoc.documentElement;
-    body = eventDoc.body;
-    
-    mouseX = event.clientX +
-      (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-      (doc && doc.clientLeft || body && body.clientLeft || 0);
-    mouseY = event.clientY +
-      (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-      (doc && doc.clientTop  || body && body.clientTop  || 0 );
-
+  function getMousePos() {
+    document.onmousemove = function() {
+      event = event || window.event;
+      eventDoc = (event.target && event.target.ownerDocument) || document;
+      doc = eventDoc.documentElement;
+      body = eventDoc.body;
+      
+      mouseX = event.clientX +
+        (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+        (doc && doc.clientLeft || body && body.clientLeft || 0);
+      mouseY = event.clientY +
+        (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+        (doc && doc.clientTop  || body && body.clientTop  || 0 );
+  
+    }
   }
-  p.style.color = "black";
-  p.style.position = "absolute";
-  console.log(mouseX + " : " + mouseY);
-  p.style.left = mouseX; 
-  p.style.top = mouseY;
   
   var el;
   function addNewElement(elem, content) {
@@ -30,6 +29,15 @@ function main() {
     el.appendChild(document.createTextNode(content));
     document.body.appendChild(el);
     return el;
+  }
+  
+  function mainLoop() {
+    p.style.color = "blue";
+    p.style.position = "absolute";
+    getMousePos();
+    console.log(mouseX + " : " + mouseY);
+    p.style.left = mouseX; 
+    p.style.top = mouseY;
   }
   
 }
