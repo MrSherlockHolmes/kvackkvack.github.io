3e9d99b0ea941347cca10606fa927c672ca356ce
kvackkvack <friskarthur@gmail.com>
2015-11-14 20:53:17 +0100

    Update main.js

--- projects/htmlproject/js/main.js
+++ projects/htmlproject/js/main.js
@@ -1,6 +1,26 @@
 window.onload = main;
 function main() {
-  var player = document.createElement("p");
-  player.appendChild(document.createTextNode("player"));
+  var p = document.createElement("p");
+  p.id = 'player'
+  p.appendChild(document.createTextNode("player"));
   document.body.appendChild(player);
+  p = document.getElementById('player');
+  
+  var eventDoc, doc, body;
+  document.onmousemove = function() {
+    event = event || window.event;
+    eventDoc = (event.target && event.target.ownerDocument) || document;
+    doc = eventDoc.documentElement;
+    body = eventDoc.body;
+    
+    mouseXPos = event.clientX +
+      (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+      (doc && doc.clientLeft || body && body.clientLeft || 0);
+    mouseYPos= event.clientY +
+      (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+      (doc && doc.clientTop  || body && body.clientTop  || 0 );
+
+  }
+  p.position(mouseX,mouseY);
+  
 }
