a2c627afdfb099e477bc292be0500a0a4ec213e0
kvackkvack <friskarthur@gmail.com>
2015-11-15 07:15:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var content = ["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack.  I prefer not to give out my password.",
 "This is the second page of this post."]
 var current = 0;
-var change;
+var change, replace;
 console.log("k");
 
 setInterval(changeContents, 1000*3.5)
@@ -13,6 +13,8 @@ function changeContents() {
     current=0;
   }
   change = document.getElementById('hi');
-  change.removeChild(change.childNodes[1]);
-  
+  change.removeChild(change.childNodes[2]);
+  replace = document.createElement('p');
+  replace.appendChild(document.createTextNode(content[current]));
+  change.appendChild(replace);
 }
