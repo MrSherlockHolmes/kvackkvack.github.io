f49479e34d6d0f7014e5fbc982759f7290386390
kvackkvack <friskarthur@gmail.com>
2015-11-15 16:40:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,58 +7,39 @@ var op, timer;
 var maintimer;
 var elements;
 
-maintimer = setInterval(changeContents, 1000*6)
+maintimer = setInterval(function() {
+  current++;
+  if(current>1) {
+    current=0;
+  } else if(current<0) {
+    current=1;
+  }
+  change(0, content[0], current);
+}, 1000*6)
 
 window.onload = function() {
-  document.getElementsByClassName('next')[0].onclick = function() {
+  $('.next').eq(0).onclick = function() {
     next(0);
   }
-    
-  document.getElementsByClassName('prev')[0].onclick = function() {
+  $('.prev').eq(0).onclick = function() {
     prev(0);
   }
 }
 
-function fadeOut(element, finished) {
-  op = 1;
-  timer = setInterval(function() {
-    if(op < 0.1) {
-      clearInterval(timer);
-      finished();
-    }
-    element.style.opacity = op;
-    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-    op-=0.1;
-  }, 75)
-}
-  
-function fadeIn(element, finished) {
-  op = 0.1;
-  timer = setInterval(function() {
-    if(op >= 1) {
-      clearInterval(timer);
-      finished();
-    }
-    element.style.opacity = op;
-    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-    op+=0.1;
-  }, 75)
-}
-
 function change(elemindx, contents, indx) {
-  changeElem = $('.text-content').eq(0);
+  changeElem = $('.text-content').eq(elemindx);
   //changeElem = document.getElementsByClassName('text-content')[elemindx];
   changeElem.fadeOut(400, function() {
     changeElem.text(contents[indx]);
     changeElem.fadeIn(400, function() {
       maintimer = setInterval(changeContents, 1000*6);
       //changeElem = document.getElementsByClassName('indexnav')[elemindx];
-      changeElem = $('.indexnav')[elemindx]
+      changeElem = $('.indexnav').eq(elemindx);
       changeElem.replaceWith("<span class=\"prev\"> < </span>" + String(indx+1) + "/" + String(contents.length) + "<span class=\"next\"> > </span>");
-      document.getElementsByClassName('next')[elemindx].onclick = function() {
+      $('.next').eq(elemindx).onclick = function() {
         next(elemindx);
       }
-      document.getElementsByClassName('prev')[elemindx].onclick = function() {
+      $('.prev').eq(elemindx).onclick = function() {
         prev(elemindx);
       }
     })
@@ -81,12 +62,3 @@ function prev(indx) {
   change(indx, content[indx], current);
 }
 
-function changeContents() {
-  current++;
-  if(current>1) {
-    current=0;
-  } else if(current<0) {
-    current=1;
-  }
-  change(0, content[0], current);
-}
