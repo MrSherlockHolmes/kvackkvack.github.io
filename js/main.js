d340e0dbe4003b556ea02b950948a41756f814eb
kvackkvack <friskarthur@gmail.com>
2015-11-15 11:21:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,6 +19,10 @@ function changeContents() {
     change = document.getElementsByClassName('text-content')[elemindx];
     fadeOut(change, function() {
     change.innerHTML = contents[indx];
+    window.setTimeout(function() {
+      change = document.getElementsByClassName('indexnav')[elemindx];
+      change.innerHTML = "< " + String(indx+1) + "/" + String(contents.length) + " >";
+    }, 75) 
     change = document.getElementsByClassName('indexnav')[elemindx];
     change.innerHTML = "< " + String(indx+1) + "/" + String(contents.length) + " >";
     fadeIn(change, function() {
