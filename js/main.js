e0b4cee2ee889b86bec27c1c54a350e16cade0fd
kvackkvack <friskarthur@gmail.com>
2015-11-15 11:19:10 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var content = ["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack.  I prefer not to give out my password.",
-"I'm a 12 year old boy. I love programming. I like a lot of things. I dislike about the same amount of things. I made this website, mostly to host any JS projects I decide to make."]
+var content = [["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack.  I prefer not to give out my password.",
+"I'm a 12 year old boy. I love programming. I like a lot of things. I dislike about the same amount of things. I made this website, mostly to host any JS projects I decide to make."]]
 var current = 0;
 var change, replace;
 var op, timer;
@@ -12,15 +12,19 @@ function changeContents() {
   if(current>1) {
     current=0;
   }
-  change = document.getElementsByClassName('text-content')[0];
-  fadeOut(change, function() {
-    change.innerHTML = content[current];
+  
+  change(0, content[0], current);
+  
+  function change(elemindx, contents, indx) {
+    change = document.getElementsByClassName('text-content')[elemindx];
+    fadeOut(change, function() {
+    change.innerHTML = contents[indx];
+    change = document.getElementsByClassName('indexnav')[elemindx];
+    change.innerHTML = "< " + String(indx+1) + "/" + String(contents.length) + " >";
     fadeIn(change, function() {
-      change = document.getElementsByClassName('indexnav')[0];
-      change.innerHTML = "< " + String(current+1) + "/" + String(content.length) + " >";
     })
   });
-  
+  }
   function fadeOut(element, finished) {
     op = 1;
     timer = setInterval(function() {
