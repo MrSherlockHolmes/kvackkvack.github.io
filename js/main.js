2e0a57756a4721bd3b1cdea996f6cd05a078a068
kvackkvack <friskarthur@gmail.com>
2015-11-15 06:49:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-var content = ["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack. 
-I prefer not to give out my password.",
+var content = ["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack.  I prefer not to give out my password.",
 "This is the second page of this post."]
 var current = 0;
 var change;
