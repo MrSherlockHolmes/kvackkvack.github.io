f05526e9df87efef5bc8b0509f761cd3aec9ad61
kvackkvack <friskarthur@gmail.com>
2015-11-15 11:15:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
+var content = ["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack.  I prefer not to give out my password.",
+"I'm a 12 year old boy. I love programming. I like a lot of things. I dislike about the same amount of things. I made this website, mostly to host any JS projects I decide to make."]
+var current = 0;
+var change, replace;
+var op, timer;
+var maintimer;
 
 maintimer = setInterval(changeContents, 1000*6)
 
 function changeContents() {
-  var content = ["My name is Arthur. I prefer not to give out my last name. Online, I call myself kvackkvack.  I prefer not to give out my password.",
-  "I'm a 12 year old boy. I love programming. I like a lot of things. I dislike about the same amount of things. I made this website, mostly to host any JS projects I decide to make."]
-  var current = 0;
-  var change, replace;
-  var op, timer;
-  var maintimer;
   current++;
   if(current>1) {
     current=0;
